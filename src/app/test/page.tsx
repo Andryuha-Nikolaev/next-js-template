274d41e71712fe76ec5351ec346456cf598301ed
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2024-12-15 20:27:30 +0300

    #add hide test page in prod

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,12 +1,18 @@
 import type { Metadata } from "next";
+import { notFound } from "next/navigation";
 
 import { getPage } from "@/services/customPageService";
+import { isProd } from "@/utils/environment/isProd";
 
 import TestContent from "./components/content/TestContent";
 
 export const revalidate = 0;
 
 export async function generateMetadata(): Promise<Metadata> {
+	if (isProd()) {
+		notFound();
+	}
+
 	const data = await getPage("1");
 
 	const {
