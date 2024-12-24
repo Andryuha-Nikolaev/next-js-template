09fad74f7c20a4afa635799c90d0a2b9d908e3a4
Andrey Nikolaev <andryuha_nikolaev@mail.ru>
2024-12-24 13:12:31 +0300

    #add docker config

--- src/services/customPageService.ts
+++ src/services/customPageService.ts
@@ -4,7 +4,7 @@ import { axiosClassic } from "@/api/interceptors";
 import type { CustomPage } from "@/types/customPage";
 
 export const getPage = cache(async (slug: string) => {
-	const res = await axiosClassic<CustomPage>(`custom-page/${slug}`);
+	const res = await axiosClassic<CustomPage>(`/custom-page/${slug}`);
 
 	return res.data;
 });
