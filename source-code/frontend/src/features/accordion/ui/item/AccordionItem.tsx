0839365aa90d378d9f9b42ed8bbf9896d0d32876
Andrey Nikolaev <andryuha_nikolaev@mail.ru>
2025-06-03 14:24:29 +0300

    #add - accordion in accordion

--- source-code/frontend/src/features/accordion/ui/item/AccordionItem.tsx
+++ source-code/frontend/src/features/accordion/ui/item/AccordionItem.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import clsx from "clsx";
 
@@ -15,10 +15,32 @@ export const AccordionItem = ({
 	children,
 }: AccordionItemProps) => {
 	const { activeItem, openItem } = useAccordion();
+	const [targetHeight, setTargetHeight] = useState<number | "auto">(0);
 
 	const aRef = useRef<HTMLDivElement>(null);
 
 	const isActive = index === activeItem;
+
+	useEffect(() => {
+		if (!aRef.current) return;
+
+		const height = aRef.current.scrollHeight;
+		let timer;
+
+		if (isActive) {
+			setTargetHeight(height);
+			timer = setTimeout(() => {
+				setTargetHeight("auto");
+			}, 300);
+		} else {
+			setTargetHeight(height);
+			timer = setTimeout(() => {
+				setTargetHeight(0);
+			}, 10);
+		}
+
+		return () => clearTimeout(timer);
+	}, [isActive]);
 
 	return (
 		<div className={s.block}>
@@ -26,15 +48,12 @@ export const AccordionItem = ({
 				className={clsx(s.question, isActive && s.active)}
 				onClick={() => openItem(index)}
 			>
-				<p className={s.qText}>{question}</p>
+				<span className={s.qText}>{question}</span>
 				<span className={clsx(s.icon, isActive && s.active)}>
 					<CloseIcon />
 				</span>
 			</button>
-			<div
-				className={s.answer}
-				style={{ height: isActive ? aRef.current?.scrollHeight || 0 : 0 }}
-			>
+			<div className={s.answer} style={{ height: targetHeight }}>
 				<div ref={aRef} className={s.content}>
 					{children}
 				</div>
