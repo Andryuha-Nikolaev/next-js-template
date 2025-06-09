baccf74711162650d056d8dddfb9f6fd63576220
Andrey Nikolaev <andryuha_nikolaev@mail.ru>
2025-06-09 10:36:20 +0300

    #add - web manifest

--- source-code/frontend/src/shared/constants/meta.ts
+++ source-code/frontend/src/shared/constants/meta.ts
@@ -8,52 +8,47 @@ const metaConstants: Metadata = {
 		template: `%s | ${SITE_NAME}`,
 	},
 	description: "Description",
+	// manifest: "/web.manifest.json",
 	icons: {
 		icon: "/favicon.ico",
-		// apple: {
-		// 	rel: "apple-touch-icon",
-		// 	type: "image/png",
-		// 	sizes: "180x180",
-		// 	url: "/apple-touch-icon.png",
-		// },
-		// other: [
-		// 	{
-		// 		rel: "icon",
-		// 		type: "image/png",
-		// 		sizes: "32x32",
-		// 		url: "/favicon-32x32.png",
-		// 	},
-		// 	{
-		// 		rel: "icon",
-		// 		type: "image/png",
-		// 		sizes: "64x64",
-		// 		url: "/favicon-64x64.png",
-		// 	},
-		// 	{
-		// 		rel: "icon",
-		// 		type: "image/png",
-		// 		sizes: "16x16",
-		// 		url: "/favicon-16x16.png",
-		// 	},
-		// 	{
-		// 		rel: "icon",
-		// 		type: "image/png",
-		// 		sizes: "128x128",
-		// 		url: "/android-chrome-128x128.png",
-		// 	},
-		// 	{
-		// 		rel: "icon",
-		// 		type: "image/png",
-		// 		sizes: "192x192",
-		// 		url: "/android-chrome-192x192.png",
-		// 	},
-		// 	{
-		// 		rel: "icon",
-		// 		type: "image/png",
-		// 		sizes: "512x512",
-		// 		url: "/android-chrome-512x512.png",
-		// 	},
-		// ],
+		apple: {
+			rel: "apple-touch-icon",
+			type: "image/png",
+			sizes: "180x180",
+			url: "/apple-touch-icon.png",
+		},
+		other: [
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "32x32",
+				url: "/favicon-32x32.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "64x64",
+				url: "/favicon-64x64.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "128x128",
+				url: "/favicon-128x128.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "192x192",
+				url: "/favicon-192x192.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "512x512",
+				url: "/favicon-512x512.png",
+			},
+		],
 	},
 	openGraph: {
 		// images: `${process.env["NEXT_PUBLIC_SITE_URL"]}/og-image.png`,
