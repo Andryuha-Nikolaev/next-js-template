78a0218f3b74625a6ab14169ed42ed979ff5f696
Andrey Nikolaev <andryuha_nikolaev@mail.ru>
2025-06-24 11:45:20 +0300

    #fix - buttons

--- source-code/frontend/src/shared/ui/buttons/close/ui/CloseButton.tsx
+++ source-code/frontend/src/shared/ui/buttons/close/ui/CloseButton.tsx
@@ -7,18 +7,22 @@ import { CloseIcon } from "./icons/close-icon/CloseIcon";
 
 type CloseButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
 	className?: string;
+	colorVariant?: "transparent" | "var1" | "var2" | "var3";
+	size?: "sm" | "md" | "lg" | "auto";
 };
 
 export const CloseButton = ({
 	onClick,
 	className,
 	children,
+	colorVariant = "transparent",
+	size = "auto",
 	...restProps
 }: CloseButtonProps) => {
 	return (
 		<button
 			onClick={onClick}
-			className={clsx(s.button, className)}
+			className={clsx(s.button, className, s[colorVariant], s[size])}
 			aria-label="Закрыть"
 			type="button"
 			{...restProps}
